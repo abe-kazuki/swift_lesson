92f5bae8333351f35626c4da5afd3b867d8e0561
Kazuki Abe <kazuki.abe@jmty.jp>
2020-04-16 13:38:21 +0900

    FizzBuzz関連のplaygroundです。

---
+++ FizzBuzz/FizzBuzz.playground/Contents.swift
@@ -0,0 +1,16 @@
+//FizzBuzz Ver.orizin
+for number in 1...100{
+    if number % 15 == 0{
+        print("FizzBuzz")
+    }
+    else if number % 3 == 0{
+        print("Fizz")
+    }
+    else if number % 5 == 0{
+        print("Buzz")
+    }
+    else{
+        print(number)
+    }
+}
+
