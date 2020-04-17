47e622e03e878ddf604c33ddc4a730bd3776f0b1
Kazuki Abe <kazuki.abe@jmty.jp>
2020-04-17 17:58:46 +0900

    コメントを基に修正。IBActionは使わずに実装

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,53 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    enum operatype: Int{
+        case add = 0
+        case sub = 1
+        case mul = 2
+        case div = 3
+    }
     
     @IBOutlet weak var textfield1: UITextField!
     @IBOutlet weak var textfield2: UITextField!
-
     
     @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var btn1: UIButton!
     
-    var selected_operator: Int = 0
+    @IBOutlet weak var segment: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
         label1.text = "0"
-    }
-    
-    @IBAction func changevalue(_ sender: UISegmentedControl) {
+        btn1.setTitle("計算開始" , for: .normal)
+        btn1.addTarget(self, action: #selector(pressed_button(_:)), for: UIControl.Event.touchUpInside)
 
-        selected_operator = sender.selectedSegmentIndex
     }
     
-    
-    
-    @IBAction func pressbutton(_ sender: Any) {
-        let num1 = (textfield1.text! as NSString).integerValue
-        let num2 = (textfield2.text! as NSString).integerValue
-        
-        var result = 0
-        
-        switch selected_operator {
-        case 0:
-            result = num1 + num2
-            break
-        case 1:
-            result = num1 - num2
-            break
-        case 2:
-            result = num1 * num2
-            break
-        case 3:
-            result = num1 / num2
-            break
-        default:
-            break
+    @objc func pressed_button(_ senfer: UIButton){
+        let num1 = Int(textfield1.text!) ?? 0
+        let num2 = Int(textfield2.text!) ?? 0
+
+        var rerult: Int{
+            switch segment.selectedSegmentIndex {
+            case operatype.add.rawValue:
+                return num1 + num2
+            case operatype.sub.rawValue:
+                return num1 - num2
+            case operatype.mul.rawValue:
+                return num1 * num2
+            case operatype.div.rawValue where num2 != 0 :
+                return num1 / num2
+            default:
+                return 0
+            }
         }
-        
-        label1.text = "結果：\(result)"
+        label1.text = "結果：\(rerult)"
+
     }
- 
- }
+
+}
